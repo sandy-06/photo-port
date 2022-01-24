5c80b07efa687c12f969838654c8a298d63325a4
Sandra Sanders <sandy-sanders@msn.com>
2022-01-24 12:55:50 -0700

    first test

---
+++ src/components/__tests__/index.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom.extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //renders About test
+    //First Test
+    it('renders', () => {
+        render(<About />);
+    });
+
+    //Second Test
+})
