e0298c972c00f26380efadfa2367249532913870
Sandra Sanders <sandy-sanders@msn.com>
2022-01-24 13:21:04 -0700

    abouttest

--- src/components/__tests__/index.test.js
+++ src/components/__tests__/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom.extend-expect';
-import About from '..';
+import '@testing-library/jest-dom/extend-expect';
+import About from '../About';
 
 afterEach(cleanup);
 
@@ -12,5 +12,9 @@ describe('About component', () => {
         render(<About />);
     });
 
-    //Second Test
-})
+    //snapshot test
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+      })
+});
